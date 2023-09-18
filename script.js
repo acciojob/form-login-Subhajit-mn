f90f09c5f71ff48b2c75a89ec798afc43cae53e5
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-09-18 20:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let fName = document.getElementByname("fname");
+	let lName = document.getElementByname("lname");
+	alert(console.log(fName, lName));
 }
